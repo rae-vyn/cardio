ca986b9a274f1de82d144e81256cbab5cd6e7a8f
raevyn <raevyntunes@outlook.com>
2024-05-10 07:11:04 -0400

    added testing function

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,24 @@
 use cardio::{Card, Deck};
 
-fn _scenario(condition: fn(Card) -> bool, deck: Deck) -> bool {
-    for card in deck.cards() {
-        return condition(card);
-    }
-    return false;
+fn run_scenario(condition: fn(Card) -> bool, deck: Deck, iterations: u16) -> f32 {
+    (0u16..iterations).into_iter()
+        .map(|_| deck.pick_random())
+        .map(|x| condition(x))
+        .filter(|x| *x)
+        .collect::<Vec<bool>>()
+        .len() as f32 / iterations as f32
 }
-
-fn is_king(card: Card) -> bool {
-    card.card_type() == cardio::CardType::King
+fn is_face(card: Card) -> bool {
+    match card.card_type() {
+        cardio::CardType::Number(_) | cardio::CardType::Ace => false,
+        _ => true
+    }
 }
 
 fn main() {
     let jokerless_deck = Deck::full_no_jokers();
 
-    let runs: Vec<bool> = (1u8..=100)
-        .map(|_| is_king(jokerless_deck.pick_random()))
-        .collect();
-
-    let trues = runs
-        .clone()
-        .into_iter()
-        .filter(|x| *x)
-        .collect::<Vec<bool>>()
-        .len() as f32;
-    let falses = runs
-        .clone()
-        .into_iter()
-        .filter(|x| !*x)
-        .collect::<Vec<bool>>()
-        .len() as f32;
-    println!("{} {}", trues, falses);
-
-    println!("Chances of getting a king: {:#?}", (trues/100f32) as f32);
+    let runs = run_scenario(is_face, jokerless_deck, 1000);
+    
+    println!("Chances of getting a face card: {:#?} ({}%)", runs, runs*100.0);
 }
